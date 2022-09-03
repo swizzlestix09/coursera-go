a2b294dffe16d326579bacd4d93009b365af12ce
swizzlestix09 <angely.rodriguez@gmail.com>
2022-09-03 14:51:55 -0400

    started slice.go

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coursera-go
+
+This repo is dedicated to all course work involved in Coursera's Getting started with Go.
+
+Course: https://www.coursera.org/learn/golang-getting-started/home/info
---
+++ slice.go
@@ -0,0 +1,26 @@
+// Write a program which prompts the user to enter integers and stores the integers in a sorted slice.
+// The program should be written as a loop.
+// Before entering the loop, the program should create an empty integer slice of size (length) 3.
+// During each pass through the loop, the program prompts the user to enter an integer to be added to the slice.
+// The program adds the integer to the slice, sorts the slice, and prints the contents of the slice in sorted order.
+// The slice must grow in size to accommodate any number of integers which the user decides to enter.
+// The program should only quit (exiting the loop) when the user enters the character ‘X’ instead of an integer.
+
+// Submit your source code for the program,
+// “slice.go”.
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	//declare a empty  slice that is size 3.
+	var inputInts := make([]int, 3)
+  fmt.Println("number please..")
+	//prompt user to enter intergers
+	//store intergers in sorted slice
+
+	fmt.Println("Done picking numbers. Here they are, sorted by the computer because you're a lazy homosapien: ")
+	//if user types X quit program
+}
